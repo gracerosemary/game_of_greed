1a16a3324eacc87090ee676cf835ecf8a24df4fb
Grace Choi <choi.g.330@gmail.com>
2020-12-29 20:24:55 -0800

    frame done

---
+++ game_of_greed/game_logic.py
@@ -0,0 +1,25 @@
+from collections import Counter
+import random
+
+class GameLogic:
+    @staticmethod
+    def calculate_score(score:tuple):
+        pass
+
+    @staticmethod
+    def roll_dice(dice: int):
+        pass
+
+class Banker:
+
+    def __init__(self):
+        pass
+        
+    def shelf(self, shelf_points: int):
+        pass
+
+    def bank(self, bank_points: int):
+        pass
+
+    def clear_shelf(self):
+        pass
