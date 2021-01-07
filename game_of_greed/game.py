7abc0f2ae0f556a37f489891aeddb5c271f1af5b
Matthew Holder <holdermatthew5@gmail.com>
2021-01-06 19:57:14 -0800

    print new roll

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -33,30 +33,61 @@ class Game:
     def decline_game(self):
         print("OK. Maybe another time")
     
-    def closing(self, score):
+    def closing(self, score=0):
         print(f"Thanks for playing. You earned {score} points")
 
     def start_game(self):
         removed = 0
-        roll = GameLogic.roll_dice(6)
+        remaining = 6
+        self.round_num += 1
+        roll = GameLogic.roll_dice(remaining)
         print(f"""
-Starting round 1
-Rolling 6 dice...
+Starting round {self.round_num}
+Rolling {remaining} dice...
 *** {roll[0]} {roll[1]} {roll[2]} {roll[3]} {roll[4]} {roll[5]} ***
         """)
-        save = input("""Enter dice to keep, or (q)uit:
+        current_roll = GameLogic.calculate_score(roll)
+        if current_roll == 0:
+            print('Farkle!')
+        else:
+            remaining = remaining - removed
+            save = input("""Enter dice to keep, or (q)uit:
 > """)
         if save == 'q':
             self.closing()
         else:
             print(save)
-            tup = tuple([int(save)])
+            dice = str([int(i) for i in remaining])
+            tup = tuple([int(i) for i in save])
             score = GameLogic.calculate_score(tup)
             print(tup)
             removed += len(tup)
-            print(f'You have {score} unbanked points and {6 - removed} dice remaining')
+            while removed != 0:
+                print(f'You have {score} unbanked points and {6 - removed} dice remaining')
+                roll_again = input("""(r)oll again, (b)ank your points or (q)uit:
+> 
+                """)
+                if roll_again == 'r':
+                    if current_roll == 0:
+                        print('Farkle!')
+                        Banker.shelved = 0
+                        self.round_num += 1
+                    else:
+                        roll = GameLogic.roll_dice(remaining)
+                        print(dice)
+                        print(f"""Rolling {remaining} dice...
+*** {dice} ***
+)""")
+                    
+                elif roll_again == 'b':
+                    Banker.bank
+                elif roll_again == 'q':
+                    self.closing()
+                    break
+                else:
+                    pass
 
-
+# GameLogic.calculate_score(roll)
 if __name__ == "__main__":
     game = Game()
     game.play()
