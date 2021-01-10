459ca96da6a193eca9107fe65923354fb779ba26
Grace Choi <choi.g.330@gmail.com>
2021-01-09 16:18:17 -0800

    cancel

--- tests/version_one/test_sim_advanced.py
+++ tests/version_one/test_sim_advanced.py
@@ -7,7 +7,11 @@ def test_repeat_roller():
     """Allow setting aside scoring dice and rolling the rest
     """
 <<<<<<< HEAD
+<<<<<<< HEAD
     errors = diff(Game().play, path="tests/version_3/repeat_roller.sim.txt")
+=======
+    errors = diff(Game().play, path="tests/version_one/repeat_roller.sim.txt")
+>>>>>>> e55c8fcdb3bd539212bbb2edec393ceb70a8c3ee
 =======
     errors = diff(Game().play, path="tests/version_one/repeat_roller.sim.txt")
 >>>>>>> e55c8fcdb3bd539212bbb2edec393ceb70a8c3ee
@@ -17,7 +21,11 @@ def test_hot_dice():
     then user gets 6 fresh dice and continues turn.
     """
 <<<<<<< HEAD
+<<<<<<< HEAD
     errors = diff(Game().play, path="tests/version_3/hot_dice.sim.txt")
+=======
+    errors = diff(Game().play, path="tests/version_one/hot_dice.sim.txt")
+>>>>>>> e55c8fcdb3bd539212bbb2edec393ceb70a8c3ee
 =======
     errors = diff(Game().play, path="tests/version_one/hot_dice.sim.txt")
 >>>>>>> e55c8fcdb3bd539212bbb2edec393ceb70a8c3ee
@@ -28,7 +36,11 @@ def test_cheat_and_fix():
     If invalid prompt user for re-entry
     """
 <<<<<<< HEAD
+<<<<<<< HEAD
     errors = diff(Game().play, path="tests/version_3/cheat_and_fix.sim.txt")
+=======
+    errors = diff(Game().play, path="tests/version_one/cheat_and_fix.sim.txt")
+>>>>>>> e55c8fcdb3bd539212bbb2edec393ceb70a8c3ee
 =======
     errors = diff(Game().play, path="tests/version_one/cheat_and_fix.sim.txt")
 >>>>>>> e55c8fcdb3bd539212bbb2edec393ceb70a8c3ee
@@ -40,7 +52,11 @@ def test_zilcher():
     and ends turn
     """
 <<<<<<< HEAD
+<<<<<<< HEAD
     errors = diff(Game().play, path="tests/version_3/zilcher.sim.txt")
+=======
+    errors = diff(Game().play, path="tests/version_one/zilcher.sim.txt")
+>>>>>>> e55c8fcdb3bd539212bbb2edec393ceb70a8c3ee
 =======
     errors = diff(Game().play, path="tests/version_one/zilcher.sim.txt")
 >>>>>>> e55c8fcdb3bd539212bbb2edec393ceb70a8c3ee
